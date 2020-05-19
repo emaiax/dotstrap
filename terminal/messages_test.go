feeb46273e024aabff8b2a1384520dd49370574e
Eduardo Maia <eduvimaia@gmail.com>
2020-05-19 18:39:58 -0300

    go fmt

--- terminal/messages_test.go
+++ terminal/messages_test.go
@@ -1,24 +1,24 @@
 package terminal
 
 import (
-  "strings"
-  "testing"
+	"strings"
+	"testing"
 
-  "github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestConfirmYes(t *testing.T) {
-  ioReader := strings.NewReader("y\n")
+	ioReader := strings.NewReader("y\n")
 
-  answer := Confirm("Yes?", ioReader)
+	answer := Confirm("Yes?", ioReader)
 
-  assert.True(t, answer)
+	assert.True(t, answer)
 }
 
 func TestConfirmNo(t *testing.T) {
-  ioReader := strings.NewReader("n\n")
+	ioReader := strings.NewReader("n\n")
 
-  answer := Confirm("Yes?", ioReader)
+	answer := Confirm("Yes?", ioReader)
 
-  assert.False(t, answer)
+	assert.False(t, answer)
 }
