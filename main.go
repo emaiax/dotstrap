bebc8f0ccf9eecf6700098ced8f844eb8bbb2b83
Eduardo Maia <eduvimaia@gmail.com>
2020-05-22 18:14:03 -0300

    extracted config setup to init

--- main.go
+++ main.go
@@ -7,38 +7,38 @@ import (
 
 	"github.com/emaiax/dotstrap/config"
 	"github.com/emaiax/dotstrap/packages"
-	"github.com/emaiax/dotstrap/terminal"
+	"github.com/emaiax/dotstrap/tty"
 )
 
 const configFile = "dotstrap.yml"
 
+var env *config.Environment
+
 func init() {
-	log.SetOutput(os.Stdout)
+  log.SetOutput(os.Stdout)
+
+  environment, err := config.Load(configFile)
+
+  // TODO: check if user has read access to env.Config.Source
+  // TODO: check if user has write access to env.Config.Target
+  //
+  if os.IsNotExist(err) {
+    fmt.Println(terminal.Bold(terminal.Error("Installer config file not found, exiting now.")))
+
+    os.Exit(-1)
+  }
+
+  if err != nil {
+    fmt.Println(terminal.Bold(terminal.Error(fmt.Sprint(err))))
+
+    os.Exit(-1)
+  }
+
+  env = environment
 }
 
 func main() {
 	terminal.Start()
-
-	env, err := config.Load(configFile)
-
-  // TODO: check if user has write access to env.Config.Target
-  // otherwise it won't be possible to install anything
-  //
-	if os.IsNotExist(err) {
-		fmt.Println(terminal.Bold(terminal.Error("Config file not found.")))
-
-		terminal.Quit()
-
-		os.Exit(-1)
-	}
-
-	if err != nil {
-		fmt.Println(terminal.Bold(terminal.Error(fmt.Sprint(err))))
-
-		terminal.Quit()
-
-		os.Exit(-1)
-	}
 
 	terminal.PrintConfigs(env.Config.Source, env.Config.Target, env.Config.DryRun)
 
